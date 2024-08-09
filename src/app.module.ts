e633a56e685a40f4b2ed37f14a3b2d6a955103d7
Larissa Angioni <larissaangioni@gmail.com>
2024-08-09 10:45:19 -0300

    feat: Deploy do CRUD Farmácia

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,21 @@ import { Categoria } from './categoria/entities/categoria.entity';
 import { CategoriaModule } from './categoria/categoria.module';
 import { Produto } from './produto/entities/produto.entity';
 import { ProdutoModule } from './produto/produto.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'db_farmacia',
-    entities: [Categoria, Produto],
-    synchronize: true,
-  }),
-  CategoriaModule,
-  ProdutoModule,
-],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
+    CategoriaModule,
+    ProdutoModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
